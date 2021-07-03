a2b6bda65a7cc2dff3fb9e08fb6fc0efb99c8efc
JrzenonDev <jrobertoonb@gmail.com>
2021-07-03 00:51:26 -0300

    chore: organizando a parte de rotas de login e home, deixando cada componente com sua rota e arquivo de configuração individual

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import routes from './routes'
 
 Vue.use(Router)
 
-export default new Router({
-
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import(/* webpackChunkName: "home" */ '../pages/Home')
-    }
-  ]
-})
+export default new Router({ routes })
